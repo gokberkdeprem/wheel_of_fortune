c5c7c32a79cb6158f8e2f3f8bb92a09d14f01c2d
gokberkdeprem <gkdeprem@gmail.com>
2024-11-04 00:55:41 +0300

    Implement result pop up.

--- Assets/Scripts/ZoneManager.cs
+++ Assets/Scripts/ZoneManager.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using DG.Tweening;
 using TMPro;
+using Unity.VisualScripting.Antlr3.Runtime.Tree;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.Serialization;
@@ -15,14 +16,23 @@ public class ZoneManager : MonoBehaviour
     public List<Zone> Zones = new List<Zone>();
     [SerializeField] ZoneAnimationController ZoneAnimationController;
     [SerializeField] WheelOfFortuneController WheelOfFortuneController;
+    [SerializeField] private Transform prizeTransform;
+    [SerializeField] Transform resultContent;
+    [SerializeField] GameObject resultGrid;
+    [SerializeField] private Canvas _canvas;
     public Button retryButton;
     public Button exitButton;
+    public Button collectButton;
 
     private void OnValidate()
     {
         exitButton = GameObject.Find("exit_button").GetComponent<Button>();
+        exitButton.onClick.AddListener(ExitEvent);
         retryButton = GameObject.Find("retry_button").GetComponent<Button>();
         retryButton.onClick.AddListener(Retry);
+        collectButton = GameObject.Find("collect_all").GetComponent<Button>();
+        collectButton.onClick.AddListener(Retry);
+        
     }
 
     private void Retry()
@@ -32,6 +42,7 @@ public class ZoneManager : MonoBehaviour
 
     private void Awake()
     {
+        resultGrid.SetActive(false);
         retryButton.gameObject.SetActive(false);
         Instance = this;
     }
@@ -48,5 +59,28 @@ public class ZoneManager : MonoBehaviour
     public void ShowRetryButton()
     {
         retryButton.gameObject.SetActive(true);
+    }
+
+    public void ExitEvent()
+    {
+        if(prizeTransform.childCount == 0)
+            Retry();
+        else
+        {
+            foreach (Transform child in _canvas.transform)
+            {
+                if (child != gameObject.transform || child != resultGrid.transform)
+                {
+                    child.gameObject.SetActive(false);
+                }
+            }
+            
+            
+            resultGrid.SetActive(true);
+            foreach (Transform child in prizeTransform)
+            {
+                Instantiate(child.gameObject, resultContent.transform);
+            }
+        }
     }
 }
